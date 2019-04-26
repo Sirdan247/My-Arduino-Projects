2f753fe6d54d4fec07ba607bb4b46559b7b29540
Sirdan247 <dlion9353@gmail.com>
2019-04-26 08:10:04 +0100

    my pushbotton controlled LED project

---
+++ push_button_controlled_LED/push_button_controlled_LED.ino
@@ -0,0 +1,19 @@
+// Initialize variables 
+const int buttonPin = 2; // Pin connected to pushbutton
+const int ledPin = 13; // Pin connected to LED
+int buttonState = 0; // Give pushbutton a value
+void setup() {
+  //set pinModes
+pinMode(ledPin, OUTPUT); // Set LED pin as output
+pinMode(buttonPin, INPUT); // Set pushbutton pin as input
+}
+void loop() {
+  //set operating conditions
+buttonState = digitalRead(buttonPin); // Read input from pin 2
+if (buttonState == HIGH) { // If pushbutton is pressed, set as HIGH
+digitalWrite(ledPin, HIGH); // Turn on LED
+}
+else {
+digitalWrite(ledPin, LOW); // Otherwise, turn off LED
+}
+}
